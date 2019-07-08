7f9afcb54028d7d89bcff8b5ba84ebc21c00dcb5
Neo <51684865+THEGHOTR@users.noreply.github.com>
2019-07-08 10:48:55 +1000

    Added Main Menu and basic game objects to level 1

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    /// <summary>
+    /// The scene we want to load.
+    /// </summary>
+    //[SerializeField] private int sceneIndex;
+    
+    /// <summary>
+    /// The scene to load.
+    /// </summary>
+    public void LoadScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
