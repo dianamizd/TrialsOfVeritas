25707c4de9923aa512be1bcd9ca22b1ba3bc3b7c
Neo <51684865+THEGHOTR@users.noreply.github.com>
2019-07-08 11:26:18 +1000

    Added Player 1 movement
    Created input for Player 1 movement, still need to change the speed cause its framerate independent. Also made the player character face the direction it's moving.

---
+++ Assets/Scripts/Player1Input.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Input : MonoBehaviour
+{
+    public float h;
+
+    public float v;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(h, 0, v);
+
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            print("left/right movement");
+        } 
+
+        if (Input.GetButtonDown("Vertical"))
+        {
+            print("up/down movement");
+        }
+
+        h = Input.GetAxis("Horizontal");
+
+        v = Input.GetAxis("Vertical");
+
+        transform.Translate(movement * Time.deltaTime, 0);
+
+        transform.rotation = Quaternion.LookRotation(movement);
+
+        Debug.Log(h);
+    }
+}
