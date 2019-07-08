6eeacaee6763d510d63c120fd24cd2f7a1054546
Diana <diana0mizdrak@gmail.com>
2019-07-08 22:00:27 +1000

    Added in power up
    Currently it's just a sphere in the islandlevel scene, but it does dissapear on contact with player.
    Later i plan on making it so that it's randomly generated on the map every couple of seconds.
    There's also a section commented out in the code in relation to particle effects - which we can add in later.

---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    ////variable for the effect that'll play when an object is picked up
+    //public GameObject pickupEffect;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //if the player collides with the object, Pickup() will trigger
+        if (other.CompareTag("Player"))
+        {
+            Pickup();
+        }
+    }
+
+    void Pickup()
+    {
+        ////spawn effect - like a particle effect
+        ////can uncomment when we have an effect from the asset store
+        //Instantiate(pickupEffect, transform.position, transform.rotation);
+
+        //apply effect to player
+
+        //remove powerup object
+        Destroy(gameObject);
+    }
+}
