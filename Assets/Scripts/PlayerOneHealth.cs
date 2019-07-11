441226d9f43222a528684983f59c45aea5552998
Diana <diana0mizdrak@gmail.com>
2019-07-11 22:00:52 +1000

    Added in functionality of health for both players

---
+++ Assets/Scripts/PlayerOneHealth.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerOneHealth : MonoBehaviour
+{
+    //healthbar
+    public Slider healthBar;
+
+    //setting health variables - but will need to change for each different character
+    float maxHealth = 100;
+    float currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //when the game starts, players health=max value
+        healthBar.value = maxHealth;
+        //health gets update when damage is taken
+        currentHealth = healthBar.value;
+    }
+
+    //currently, when the player collides with the other player, they'll take damage - this can be changed when projectiles are implemeted
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            healthBar.value -= 5f;
+            currentHealth = healthBar.value;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerTwoHealth.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerTwoHealth : MonoBehaviour
+{
+    //since the 2nd players UI works in reverse, it's different from player 1's script
+
+    //healthbar
+    public Slider healthBar;
+
+    //setting health variables - but will need to change for each different character
+    //float maxHealth = 100;
+    float currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //when the game starts, players health=0 (as it works in reverse)
+        healthBar.value = 0;
+        //health gets update when damage is taken
+        currentHealth = healthBar.value;
+    }
+
+    //currently, when the player collides with the other player, they'll take damage(or in player 2's case, healed) - this can be changed when projectiles are implemeted
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            healthBar.value += 5f;
+            currentHealth = healthBar.value;
+        }
+    }
+
+    //we can add something "if the player's health reaches maxHealth, they lose" but idk atm
+}
