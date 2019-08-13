529d9915c6743dd61b450df3cb7e45cbf5fe469f
Neo S <51684865+THEGHOTR@users.noreply.github.com>
2019-08-13 12:22:31 +1000

    ...

--- Assets/Scripts/CharacterSelect.cs
+++ Assets/Scripts/CharacterSelect.cs
@@ -4,49 +4,49 @@ using UnityEngine;
 
 public class CharacterSelect : MonoBehaviour
 {
-    public int playerCharacter1 = 1;
+    public string playerCharacter1;
 
-    public int playerCharacter2 = 1;
+    public string playerCharacter2;
         
 
-    private void Awake()
+    private void Start()
     {
-        playerCharacter1 = 1;
+        playerCharacter1 = "P1Archer";
 
-        playerCharacter2 = 1;
+        playerCharacter2 = "P2Archer";
             
     }
 
     public void ArcherP1()
     {
         Debug.Log("P1 Archer Selected");
-        playerCharacter1 = 1; 
+        playerCharacter1 = "P1Archer"; 
     }
 
     public void GladiatorP1()
     {
         Debug.Log("P1 Gladiator Selected");
-        playerCharacter1 = 2;
+        playerCharacter1 = "P1Gladiator";
     }
     public void WarlockP1()
     {
         Debug.Log("P1 Warlock Selected");
-        playerCharacter1 = 3;
+        playerCharacter1 = "P1Warlock";
     }
     public void ArcherP2()
     {
         Debug.Log("P2 Archer Selected");
-        playerCharacter2 = 1;
+        playerCharacter2 = "P2Archer";
     }
     public void GladiatorP2()
     {
-        Debug.Log("P1 Archer Selected");
-        playerCharacter2 = 2;
+        Debug.Log("P2 Gladiator Selected");
+        playerCharacter2 = "P2Gladiator";
     }
     public void WarlockP2()
     {
-        Debug.Log("P1 Archer Selected");
-        playerCharacter2 = 3;
+        Debug.Log("P2 Warlock Selected");
+        playerCharacter2 = "P2Warlock";
     }
 
 }
--- Assets/Scripts/Player1Spawn.cs
+++ Assets/Scripts/Player1Spawn.cs
@@ -4,17 +4,34 @@ using UnityEngine;
 
 public class Player1Spawn : MonoBehaviour
 {
-    
+    private string characterSelectP1;
+
+    [SerializeField] private GameObject player1Archer;
+    [SerializeField] private GameObject player1Gladiator;
+    [SerializeField] private GameObject player1Warlock;
+
+    [SerializeField] private Transform player1Spawn;
     
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        GameObject characterSelect = GameObject.Find("CharacterSelect");
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        characterSelectP1 = characterSelect.GetComponent<CharacterSelect>().playerCharacter1;
+
+        if(characterSelectP1 == "P1Archer")
+        {
+            Instantiate(player1Archer, player1Spawn.position, player1Spawn.rotation);
+        }
+
+        if(characterSelectP1 == "P1Gladiator")
+        {
+            Instantiate(player1Gladiator, player1Spawn.position, player1Spawn.rotation);
+        }
+
+        if(characterSelectP1 == "P1Warlock")
+        {
+            Instantiate(player1Warlock, player1Spawn.position, player1Spawn.rotation);
+        }
     }
 }
