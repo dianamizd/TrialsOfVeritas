71ca933cdef4603f86cb2b3a7cb04145ac58d0cd
Neo S <51684865+THEGHOTR@users.noreply.github.com>
2019-08-13 12:00:31 +1000

    trying again

--- Assets/Scripts/Player2Spawn.cs
+++ Assets/Scripts/Player2Spawn.cs
@@ -4,34 +4,15 @@ using UnityEngine;
 
 public class Player2Spawn : MonoBehaviour
 {
-    private string characterSelectP2;
-
-    [SerializeField] private GameObject player2Archer;
-    [SerializeField] private GameObject player2Gladiator;
-    [SerializeField] private GameObject player2Warlock;
-
-    [SerializeField] private Transform player2Spawn;
-
     // Start is called before the first frame update
     void Start()
     {
-        GameObject characterSelect = GameObject.Find("CharacterSelect");
+        
+    }
 
-        characterSelectP2 = characterSelect.GetComponent<CharacterSelect>().playerCharacter2;
-
-        if (characterSelectP2 == "P2Archer")
-        {
-            Instantiate(player2Archer, player2Spawn.position, player2Spawn.rotation);
-        }
-
-        if (characterSelectP2 == "P2Gladiator")
-        {
-            Instantiate(player2Gladiator, player2Spawn.position, player2Spawn.rotation);
-        }
-
-        if (characterSelectP2 == "P2Warlock")
-        {
-            Instantiate(player2Warlock, player2Spawn.position, player2Spawn.rotation);
-        }
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
